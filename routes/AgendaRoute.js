0b46441ad52804ae1a0ba62eaf962e0af43bbc31
geekvk <vijanakaush@gmail.com>
2022-06-22 14:53:42 +0530

    added csv upload and import functionality

--- routes/AgendaRoute.js
+++ routes/AgendaRoute.js
@@ -1,9 +1,49 @@
 import express from 'express';
 import AgendaModel from "../models/Agendas.js";
+import multer from 'multer';
+import fs from 'fs';
+import * as ExcelJs from 'exceljs';
+import csv from 'csvtojson';
+import XLXS from 'xlsx';
 
+export const fileUpload = async(req, res) => {
+    try{
+        const file = req.file;
+        console.log(file)
+        res.status(201).send("File is uploaded");
+    }catch(err){
+        res.status(400).send(err.message);
+    }
+}
+export const convertCSVTOJSON = async(req, res) => {
+    var workbook = XLXS.readFile(req.file.path);
+    var sheet = workbook.SheetNames;
+    var x=0;
+    sheet.forEach(element => {
+        var xlData = XLXS.utils.sheet_to_json(workbook.Sheets[sheet[x]]);
+        AgendaModel.insertMany(xlData, (err, data) => {
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(data);
+            }
+        })
+        x++;
+    })
+}
+const storage = multer.diskStorage({
+    destination:(req,file, cb) => {
+        cb(null, './public');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({storage:storage});
 const router = express.Router();
 
-router.route('/').get ((req, res) => {
+router.route('/').get((req, res) => {
      AgendaModel.find({}, (err, result) => {
         if(err){
             res.json(err);
@@ -32,19 +72,17 @@ router.route("/add").post((req, res) => {
 });
 
 
-router.route('/update/:id').put(async(req, res) => {
-    let itemID = req.params.id;
-    const { title } = req.body;
+router.route('/upload').post(upload.single('file'),  convertCSVTOJSON);
 
-    const updateItem = {
-        title,
+router.route('/download').get(async(res, req) => { 
+    try{
+        const file = './public/Agenda Doc.csv';
+        res.download(file);
+        console.log('downloaded');
+    }catch(err){
+        console.log(err);
     }
-    const update = await AgendaModel.findByIdAndUpdate(itemID, updateItem).then(() => {
-        res.status(200).send({status: "item updated", item: update});
-    }).catch((err) => {
-        res.json(err);
-    })
-});
+})
 
 router.route('/delete/:id').delete((req, res) => {
     let itemID = req.params.id;
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import agendaRouter from './routes/AgendaRoute.js'
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import XLXS from 'xlsx';
+import AgendaModel from './models/Agendas.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 8001;
 const app = express();
@@ -16,8 +23,32 @@ const connection = mongoose.connection;
 connection.once("open", () => {
     console.log("mongoDB is connected");
 })
+
+
 // api end points
 app.use("/agendas", agendaRouter);
+app.use("/agendas/upload", express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/download', (req, res) => {
+    // res.download('./public/Agenda Doc.csv');
+    var wb = XLXS.utils.book_new();
+    AgendaModel.find((err, data) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            var temp = JSON.stringify(data);
+            temp = JSON.parse(temp);
+            var ws = XLXS.utils.json_to_sheet(temp);
+            var down = './public/AgendaList.csv'
+            XLXS.utils.book_append_sheet(wb,ws,'Sheet1');
+            XLXS.writeFile(wb,down);
+            res.download(down);
+        }
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`SERVER RUNS ${PORT}`);
